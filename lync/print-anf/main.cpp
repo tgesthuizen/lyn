58d454ccd7d36dae3bafed39dcc6a1248f5f42ab
Tim Gesthuizen <tim.gesthuizen@yahoo.de>
2022-11-21 16:59:09 +0100

    lync: Build print-anf tool again

---
+++ lync/print-anf/main.cpp
@@ -0,0 +1,21 @@
+#include <cstdio>
+
+#include "expr.h"
+#include "passes.h"
+
+int main(int argc, char **argv) {
+  if (argc != 2) {
+    fputs("Usage: compiler <input-file>\n", stderr);
+    return 1;
+  }
+  FILE *input = fopen(argv[1], "r");
+  if (!input) {
+    fputs("Could not open input file\n", stderr);
+    return 1;
+  }
+  auto decls = lyn::parse(input);
+  auto table = lyn::scopify(decls);
+  lyn::typecheck(decls, table);
+  const auto anfctx = lyn::genanf(decls, table);
+  lyn::print_anf(*anfctx, stdout);
+}
