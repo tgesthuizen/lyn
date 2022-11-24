e0190f9b17334c3097a446f10e83a7dea62311b9
Tim Gesthuizen <tim.gesthuizen@yahoo.de>
2022-11-24 23:48:46 +0100

    lync: Add unreachable() function.

--- lync/include/meta.h
+++ lync/include/meta.h
@@ -12,6 +12,19 @@ struct derive_pack<Target, type_list<Pack...>> {
 template <template <class...> class Target, class Base>
 using derive_pack_t = typename derive_pack<Target, Base>::type;
 
+// Taken verbatim from cppreference.com:
+// https://en.cppreference.com/w/cpp/utility/unreachable
+[[noreturn]] inline void unreachable() {
+  // Uses compiler specific extensions if possible.
+  // Even if no extension is used, undefined behavior is still raised by
+  // an empty function body and the noreturn attribute.
+#ifdef __GNUC__ // GCC, Clang, ICC
+  __builtin_unreachable();
+#elifdef _MSC_VER // MSVC
+  __assume(false);
+#endif
+}
+
 } // namespace lyn
 
 #endif
