8e9629c4f804de9f5024d2209de6130eb06ce0dc
Tim Gesthuizen <tim.gesthuizen@yahoo.de>
2022-12-04 17:18:49 +0100

    lync: Add dead code elimination pass.

--- lync/src/anf.cpp
+++ lync/src/anf.cpp
@@ -19,6 +19,11 @@ struct fun_info {
   bool global;
 };
 
+struct local_info {
+  int ref_count = 0;
+  std::variant<std::monostate, int, std::string_view> rewritable;
+};
+
 class anf_generator {
 public:
   explicit anf_generator(string_table &stbl, const symbol_table &symtab)
@@ -30,6 +35,9 @@ public:
 
   void run();
   int get_next_id() const { return next_id; }
+  std::unordered_map<int, local_info> get_local_infos() && {
+    return std::move(local_infos);
+  }
   anf_context &&get_context() && { return std::move(ctx); }
 
 private:
@@ -39,10 +47,6 @@ private:
   const symbol_table &symtab;
   int next_id;
 
-  struct local_info {
-    int ref_count = 0;
-    std::variant<std::monostate, int, std::string_view> rewritable;
-  };
   std::unordered_map<int, local_info> local_infos;
   std::vector<fun_info> funcs_to_generate = {};
   anf_context ctx = {};
@@ -103,8 +107,11 @@ int anf_generator::visit_expr(const lyn::expr &value) {
       std::vector<int> args;
       args.reserve(std::size(expr.args));
       std::transform(std::begin(expr.args), std::end(expr.args),
-                     std::back_inserter(args),
-                     [this](lyn::expr *arg) { return visit_expr(*arg); });
+                     std::back_inserter(args), [this](lyn::expr *arg) {
+                       const int local_id = visit_expr(*arg);
+                       ++local_infos[local_id].ref_count;
+                       return local_id;
+                     });
       tail_pos = tail_pos_saved;
       int call_id = 0;
       if (!tail_pos)
@@ -113,7 +120,8 @@ int anf_generator::visit_expr(const lyn::expr &value) {
       if (std::holds_alternative<std::string_view>(
               local_infos[fid].rewritable)) {
         target = std::get<std::string_view>(local_infos[fid].rewritable);
-        --local_infos[fid].ref_count;
+      } else {
+        ++local_infos[fid].ref_count;
       }
       current_block->content.emplace_back(
           anf_call{target, std::move(args), call_id, tail_pos});
@@ -143,6 +151,7 @@ int anf_generator::visit_expr(const lyn::expr &value) {
     if constexpr (std::is_same_v<expr_t, if_expr>) {
       const auto tail_pos_saved = std::exchange(tail_pos, false);
       const int cond_id = visit_expr(*expr.cond);
+      ++local_infos[cond_id].ref_count;
       tail_pos = tail_pos_saved;
       // Please be very aware in the below section that inserting into
       // current_def->blocks might get you a dangling current_block
@@ -182,6 +191,57 @@ int anf_generator::visit_expr(const lyn::expr &value) {
   return std::visit(visit_fun, value.content);
 }
 
+struct anf_dead_code_elim {
+  bool can_be_deleted(const anf_expr &expr);
+  void run();
+
+  std::unordered_map<int, local_info> local_infos;
+  anf_context ctx;
+};
+
+bool anf_dead_code_elim::can_be_deleted(const anf_expr &expr) {
+  return std::visit(
+      [this](auto &&expr) {
+        using expr_t = std::decay_t<decltype(expr)>;
+        if (std::is_same_v<expr_t, anf_receive>)
+          // Receives cannot be deleted, they are required for stack management
+          return false;
+        if (std::is_same_v<expr_t, anf_adjust_stack>)
+          return false;
+        if constexpr (std::is_same_v<expr_t, anf_global>)
+          return local_infos[expr.id].ref_count == 0;
+        if constexpr (std::is_same_v<expr_t, anf_constant>)
+          return local_infos[expr.id].ref_count == 0;
+        if (std::is_same_v<expr_t, anf_call>)
+          // TODO: We could inspect the call for being side-effect free here
+          return false;
+        if constexpr (std::is_same_v<expr_t, anf_assoc>)
+          return local_infos[expr.id].ref_count == 0;
+        if (std::is_same_v<expr_t, anf_cond>)
+          return false;
+        if (std::is_same_v<expr_t, anf_return>)
+          return false;
+        if (std::is_same_v<expr_t, anf_jump>)
+          return false;
+        if (std::is_same_v<expr_t, anf_global_assign>)
+          return false;
+      },
+      expr);
+}
+
+void anf_dead_code_elim::run() {
+  for (auto &&def : ctx.defs) {
+    for (auto &&block : def.blocks) {
+      const auto block_end = std::end(block.content);
+      block.content.erase(std::remove_if(std::begin(block.content), block_end,
+                                         [this](const anf_expr &expr) {
+                                           return can_be_deleted(expr);
+                                         }),
+                          block_end);
+    }
+  }
+}
+
 } // namespace
 
 std::unique_ptr<anf_context, delete_anf>
@@ -199,8 +259,12 @@ genanf(std::vector<toplevel_expr> &exprs, string_table &stbl,
     gen.push_func(expr.name, &std::get<lambda_expr>(expr.value->content));
   }
   gen.run();
+  anf_dead_code_elim eliminator{std::move(gen).get_local_infos(),
+                                std::move(gen).get_context()};
+  eliminator.run();
+
   return std::unique_ptr<anf_context, delete_anf>{
-      new anf_context{std::move(gen).get_context()}};
+      new anf_context{std::move(eliminator.ctx)}};
 }
 
 void delete_anf::operator()(anf_context *ctx) { delete ctx; }
